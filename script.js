34b4d0e3e43cb194c94c0959337d8964995b5ce0
Anorakkkkk <maurichkonrad@gmail.com>
2026-03-15 17:51:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click", () => {
+  mainNav.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+  if (!menuToggle.contains(event.target) && !mainNav.contains(event.target)) {
+    mainNav.classList.remove("active");
+  }
+});
